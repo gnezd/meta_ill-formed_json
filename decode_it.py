717ed02426b630d3c6ccb02c157e2d888a272327
YC Dzeng <ycdzneg@gmail.com>
2023-07-16 13:39:01 +0800

    吃post以外的資料類型

--- decode_it.py
+++ decode_it.py
@@ -2,12 +2,19 @@ import json
 raw = open('./your_posts_1.json', 'r').read()
 parsed = json.loads(raw)
 
+# Iterate through all entries
 for nth in range(len(parsed)):
-  #print(str(parsed[nth]).encode('latin1').decode('utf-8'))
+  print('entry {}'.format(nth))
+  # If data exists?
   if 'data' in parsed[nth]:
-    if len(parsed[nth]['data']) > 0:
-      if 'post' in parsed[nth]['data'][0]:
-        print(parsed[nth]['data'][0]['post'].encode('latin1').decode('utf-8'))
-      else:
-        print('Some other data in no. {}'.format(nth))
+    # For every data entry (list)
+    for i in range(len(parsed[nth]['data'])):
+      # Every entry in the data entry list (繞口令ㄟ)
+      for tag in parsed[nth]['data'][i]:
+        print('tag: {}'.format(tag))
+        # 如果是個字串那當作臉書版壞掉兩倍寬UTF-8處理
+        if type(parsed[nth]['data'][i][tag]) == str:
+          print(parsed[nth]['data'][i][tag].encode('latin1').decode('utf-8'))
+        else:
+          print(parsed[nth]['data'][i][tag])
   print("----------")
